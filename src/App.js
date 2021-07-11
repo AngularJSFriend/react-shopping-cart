495f4cf621d8f76c8fe5f7a671e7afda63b8c343
Raka_programer <49558565+Rakacepm@users.noreply.github.com>
2021-07-11 17:42:11 +0530

    pricing-change

--- src/App.js
+++ src/App.js
@@ -1,20 +1,40 @@
 //ffeature 1
 import React from 'react';
+import data from  './data.json'
+import Products from './components/products';
 
-function App() {
+class App extends React.Component {
+  constructor(){
+super();
+this.state = {
+  products:data.products,
+  size:"",
+  sort:"",
+
+};
+  }
+
+  render(){
   return (
     <div className="grid-container">
       <header>
         <a href="/">React Shopping Cart</a>
       </header>
       <main>
-        Product List
+       <div className="content">
+       <div className="main"> 
+       <Products products = {this.state.products}></Products>
+        </div>
+      <div className="sidebar">Cart Items</div>
+       </div>
+       
       </main>
       <footer>
         All Right Reserved.
       </footer>
     </div>
   );
+}
 }
 
 export default App;
